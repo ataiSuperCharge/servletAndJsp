8ec75cccb2de4a5c406062d736383532028ab9ca
ataiSuperCharge <92355473+ataiSuperCharge@users.noreply.github.com>
2022-01-24 03:02:49 +0300

    Add files via upload
    finished jsp, servlet and view of wed application

--- src/main/java/app/servlets/ListServlet.java
+++ src/main/java/app/servlets/ListServlet.java
@@ -1,5 +1,7 @@
 package app.servlets;
 
+import app.model.Model;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,12 +10,19 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.rmi.ServerException;
+import java.util.List;
 
 public class ListServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServerException, IOException, ServletException {
+        Model model = Model.getInstance();
+        List<String> names = model.list();
+        req.setAttribute("userNames", names);
+
         RequestDispatcher requestDispatcher = req.getRequestDispatcher("views/add.jsp");
         requestDispatcher.forward(req, resp);
     }
+
+
 }
